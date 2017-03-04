720f0fdcd0f424cc63f7784032585b8b20af1c6f
Rafa Marques <RafaMarques179@googlemail.com>
2017-03-04 18:33:29 +0100

    CollisionMap class update

--- js/CollisionMap.js
+++ js/CollisionMap.js
@@ -1,28 +1,29 @@
-//
-function CollisionMap(mapHandler) {
-    this.mapHandler = mapHandler;
-}
-
-CollisionMap.prototype.getPosition = function(isY, x, y, move) {
-    var newPosition = isY ? y : x;
-    var tryPosition = isY ? y + move : x + move;
-    
-    var destinyTileId;
-    var map = this.mapHandler.getMap();
-    var tileSize = this.mapHandler.getTileSize();
-    var notWalkableId = this.mapHandler.getNotWalkableTileIDs();
-    
-    if(isY) {
-        destinyTileId = map[(y + move) / tileSize][x / tileSize];
-    } else {
-        destinyTileId = map[y / tileSize][(x + move) / tileSize];
+class CollisionMap {
+    constructor(mapHandler) {
+        this.mapHandler = mapHandler;
     }
     
-    if(isY && jQuery.inArray(destinyTileId, notWalkableId) == -1) {
-        newPosition = y + move;
-    } else if(!isY && jQuery.inArray(destinyTileId, notWalkableId) == -1) {
-        newPosition = x + move;
+    getPosition(isY, x, y, move) {
+        var newPosition = isY ? y : x;
+        var tryPosition = isY ? y + move : x + move;
+        
+        var destinyTileId;
+        var map = this.mapHandler.getMap();
+        var tileSize = this.mapHandler.getTileSize();
+        var notWalkableId = this.mapHandler.getNotWalkableTileIDs();
+        
+        if(isY) {
+            destinyTileId = map[(y + move) / tileSize][x / tileSize];
+        } else {
+            destinyTileId = map[y / tileSize][(x + move) / tileSize];
+        }
+        
+        if(isY && jQuery.inArray(destinyTileId, notWalkableId) == -1) {
+            newPosition = y + move;
+        } else if(!isY && jQuery.inArray(destinyTileId, notWalkableId) == -1) {
+            newPosition = x + move;
+        }
+        
+        return newPosition;
     }
-    
-    return newPosition;
 }
